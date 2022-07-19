983fcc71407c3fcd806a448620b7113071296a66
Junior Santos <junior.ead@hotmail.com>
2022-07-18 22:01:12 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,23 @@
+const c = (el)=>document.querySelector(el);
+const cs = (el)=>document.querySelector(el);
+
+pizzaJson.map((item, index)=>{
+    let pizzaItem = c('.models .pizza-item').cloneNode(true);
+
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('a').addEventListener('click', (e)=>{
+        e.preventDefault(); 
+
+        c('.pizzaWindowArea').style.opacity = 0;
+        c('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(()=>{
+            c('.pizzaWindowArea').style.opacity = 1
+        }, 200);
+  
+    });
+
+    c('.pizza-area').append(pizzaItem);
+});
